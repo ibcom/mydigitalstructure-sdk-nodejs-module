b6f2804dad10f5b3c810ab9b68b1e8f2c5b651d1
ibCom <support@ibcom.biz>
2017-10-16 08:53:29 +1100

    added _util functions

--- mydigitalstructure.js
+++ mydigitalstructure.js
@@ -2,9 +2,9 @@ var _ = require('lodash')
 
 module.exports = 
 {
-	data: 	{session: undefined},
+	data: {session: undefined},
 
-	init: 	function (callBack, settings)
+	init: function (callBack, settings)
 			{
 				if (settings == undefined)
 				{	
@@ -39,12 +39,40 @@ module.exports =
 				}	
 			},
 
-	logon: 	function (callBack, settings)
+	logon: 
+			function (callBack, settings, param)
 			{
 				var https = require('https');
-				var requestData = 'logon=' + settings.logon + 
-									'&password=' + settings.password;
+				var authenticationLevel = module.exports.data.settings.authenticationLevel;
+				if (_.isUndefined(authenticationLevel) {authenticationLevel = 1});
+				var logon = settings.logon;
+				var password = settings.password;
+				var code = mydigitalstructure._util.param.get(param, 'code').value;
 
+				var data = 
+				{
+					method: 'LOGON',
+					logon: logon,
+					localtime: moment().format('D MMM YYYY HH:mm:ss')
+				}
+
+				var requestData = 'logon=' + logon +
+										'&localtime=' + moment().format('D MMM YYYY HH:mm:ss');
+									//'&password=' + settings.password;
+
+				if (authenticationLevel == 1)
+				{
+					requestData += '&passwordhash=' + mydigitalstructure._util.hash(logon + password);
+				}
+				else if (authenticationLevel == 2)
+				{
+					requestData += '&passwordhash=' + mydigitalstructure._util.hash(logon + password + module.exports.data.session.logonkey)
+				}
+				else if (authenticationLevel == 3)
+				{
+					requestData += '&passwordhash=' + mydigitalstructure._util.hash(logon + password + module.exports.data.session.logonkey + code)
+				}
+				
 				var options =
 				{
 					hostname: settings.hostname,
@@ -88,7 +116,7 @@ module.exports =
 				req.end()
 			},
 
-	send: 	function (options, data, callBack)
+	send: function (options, data, callBack)
 			{
 				var https = require('https');
 				var settings = module.exports.data.settings;
@@ -165,7 +193,8 @@ module.exports =
 				req.end()
 			},
 
-	_util: 	{
+	_util: 
+			{
 				search:  
 				{	
 					init: function ()
@@ -182,7 +211,152 @@ module.exports =
 
 						return criteria
 					}
-				}
-			}							
+				},
 
+				hash: function (data)
+				{
+					var data = "do shash'owania";
+					var crypto = require('crypto');
+					return crypto.createHash('md5').update(data).digest("hex");
+				},
+
+				doCallBack: function()
+				{
+					var param, callback, data;
+
+					if (typeof arguments[0] == 'function')
+					{
+						callback = arguments[0]
+						param = arguments[1] || {};
+						data = arguments[2]
+					}
+					else
+					{
+						param = arguments[0] || {};
+						callback = param.callback;
+						data = arguments[1];
+						delete param.callback;
+					}
+
+					if (callback)
+					{
+						callback(param, data)
+					};
+				},
+
+				onComplete: function (param)
+				{
+					if (mydigitalstructure._util.param.get(param, 'onComplete').exists)
+					{
+						var onComplete = mydigitalstructure._util.param.get(param, 'onComplete').value;
+	
+						if (mydigitalstructure._util.param.get(param, 'onCompleteWhenCan').exists)
+						{
+							param.onComplete = param.onCompleteWhenCan;
+							delete param.onCompleteWhenCan;
+						}	
+						else
+						{
+							delete param.onComplete;
+						}
+
+						onComplete(param);
+					}
+					else if (mydigitalstructure._util.param.get(param, 'onCompleteWhenCan').exists)
+					{
+						var onCompleteWhenCan = mydigitalstructure._util.param.get(param, 'onCompleteWhenCan').value;
+
+						delete param.onCompleteWhenCan;
+					
+						onCompleteWhenCan(param);
+					}
+				},
+
+	param: 	{
+					get: 		function(param, name, options)
+								{
+									if (param == undefined) {param = {}}
+									if (options == undefined) {options = {}}
+							
+									var data = {exists: false, value: options.default};
+
+									var split = options.split;
+									var index = options.index;
+									var remove = options.remove;	
+									var set = options.set;
+								
+									if (param.hasOwnProperty(name))
+									{
+										if (param[name] != undefined) {data.value = param[name]};
+										data.exists = true;
+
+										if (index !== undefined && split === undefined) {split = '-'}
+
+										if (split !== undefined)
+										{
+											if (param[name] !== undefined)
+											{	
+												data.values = param[name].split(split);
+
+												if (index !== undefined)
+												{
+													if (index < data.values.length)
+													{
+														data.value = data.values[index];
+													}
+												}
+											}	
+										}
+
+										if (remove) {delete param[name]};
+										if (set) {param[name] = data.value};
+									}
+
+									return data;
+								},
+
+					set: 		function(param, key, value, options)
+								{
+									var onlyIfNoKey = false;
+
+									if (mydigitalstructure._util.param.get(options, 'onlyIfNoKey').exists)
+									{
+										onlyIfNoKey = mydigitalstructure._util.param.get(options, 'onlyIfNoKey').value
+									}
+
+									if (param === undefined) {param = {}}
+
+									if (param.hasOwnProperty(key))
+									{
+										if (!onlyIfNoKey) {param[key] = value};
+									}
+									else
+									{
+										param[key] = value;
+									}
+										
+									return param
+								}									
+				},
+
+				logoff: 	function (param)
+				{
+					var uri = mydigitalstructure._util.param.get(param, 'uri', {"default": '/'}).value;
+					var refresh = mydigitalstructure._util.param.get(param, 'refresh', {"default": true}).value;
+
+					$.ajax(
+					{
+						type: 'POST',
+						url: '/rpc/core/?method=CORE_LOGOFF',
+						dataType: 'json',
+						async: false,
+						global: false,
+						success: function (data)
+						{
+							mydigitalstructure._scope.user = undefined;
+							if (refresh) {window.location.href = uri};
+						}
+					});
+				}			
+			}							
 }
